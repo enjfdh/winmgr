69f1edef7b5e6c6053b3bde05fad86b0fc1d6967
User <User@DESKTOP-P29HECA>
2016-08-17 18:11:21 +0100

    Add project files.

---
+++ WinMgr/WindowLocationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinMgr
+{
+    public class WindowLocationController
+    {
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+
+    }
+}
